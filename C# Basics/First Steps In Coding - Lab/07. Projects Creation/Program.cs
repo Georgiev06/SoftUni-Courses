c1bddc11ce79aacad27f70f042d975cce2b4dd7c
Georgiev06 <105492506+Georgiev06@users.noreply.github.com>
2022-05-16 18:10:18 +0300

    Upload solutions for First Steps In Coding - Lab

---
+++ C# Basics/First Steps In Coding - Lab/07. Projects Creation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07._Projects_Creation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int projectsNum = int.Parse(Console.ReadLine());
+            int hours = projectsNum * 3;
+
+
+            Console.WriteLine($"The architect {name} will need {hours} hours to complete {projectsNum} project/s.");
+        }
+    }
+}
