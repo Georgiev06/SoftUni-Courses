082144637f1ab73ff43b9dc9574e4252bc4e8628
Georgiev06 <105492506+Georgiev06@users.noreply.github.com>
2022-05-16 18:43:37 +0300

    Upload solutions First Steps In Coding

---
+++ C# Basics/1.First Steps In Coding/First Steps In Coding - Lab/03. Rectangle Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+            int result = sideA * sideB;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C# Basics/1.First Steps In Coding/First Steps In Coding - Lab/First Steps In Coding - Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace First_Steps_In_Coding___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
