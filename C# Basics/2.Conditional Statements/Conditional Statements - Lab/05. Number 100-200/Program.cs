25073973c8f843b0e65d80a41616916e56e29541
Georgiev06 <105492506+Georgiev06@users.noreply.github.com>
2022-05-18 14:42:03 +0300

    Upload solutions for Conditional Statements

---
+++ C# Basics/2.Conditional Statements/Conditional Statements - Lab/05. Number 100-200/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05._Number_100_200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (num <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
