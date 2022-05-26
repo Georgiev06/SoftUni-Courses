322629a1ba0673effbf285169466cd40c4eb397b
Georgiev06 <105492506+Georgiev06@users.noreply.github.com>
2022-05-26 16:11:08 +0300

    Upload files for For Loop - Lab

---
+++ C# Basics/4.For Loop/For Loop - Lab/02. Numbers N to 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02._Numbers_N_to_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ C# Basics/4.For Loop/For Loop - Lab/For Loop - Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace For_Loop___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
